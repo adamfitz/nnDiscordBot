d82a247187e6fcc69209207a664d75d6a2e6c029
Adam <adam@networknoob.net>
2024-03-24 05:38:25 +0000

    get guild id and members

--- bot.py
+++ bot.py
@@ -6,11 +6,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('GUILD_ID')
 
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+    for guild in client.guilds:
+        if guild.id == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name} - (id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
